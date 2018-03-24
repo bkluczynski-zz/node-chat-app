1f182de8163c89d3f4f02788b148e25e123fc2ad
Bart <bart@paybase.io>
2018-03-24 18:52:03 +0000

    added test for generating location message

--- server/test/message.test.js
+++ server/test/message.test.js
@@ -1,11 +1,20 @@
 const expect = require('expect');
-const { generateMessage } = require('../utils');
+const { generateMessage, generateLocationMessage } = require('../utils');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
     const message = generateMessage('bart', 'do your laundry');
     expect(message.from).toBe('bart');
     expect(message.text).toBe('do your laundry');
+    expect(message.createdAt).toBeA('number');
+  });
+});
+describe('generateLocationMessage', () => {
+  it('should generate correct location message', () => {
+    const message = generateLocationMessage('bart', { longitude: 1, latitude: 10 });
+    console.log('message is', message);
+    expect(message.from).toBe('bart');
+    expect(message.url).toBe('https://www.google.com/maps?q=10,1');
     expect(message.createdAt).toBeA('number');
   });
 });
